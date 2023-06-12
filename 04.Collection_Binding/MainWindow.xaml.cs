aa804438bd4d46f581a7576995e31421c909c08f
AndrichSanych <Prima_rq56@student.itstep.org>
2023-06-12 19:09:02 +0300

    Create project

--- 04.Collection_Binding/MainWindow.xaml.cs
+++ 04.Collection_Binding/MainWindow.xaml.cs
@@ -13,26 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _03.Binding
+namespace _04.Collection_Binding
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-    [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class ViewModel
-    {
-        public double Alpha { get; set; }
-        public double Red { get; set; }
-        public double Green { get; set; }
-        public double Blue { get; set; }
-
     }
 }
