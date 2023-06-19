9020ce7d2e2a22943828f66ab73823c6ba4fc898
AndrewSanych <andrich0105@gmail.com>
2023-06-19 16:31:10 +0300

    Can't finish

--- 04_LibraryBooks/MainWindow.xaml.cs
+++ 04_LibraryBooks/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,35 @@ namespace _04_LibraryBooks
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new ViewModel();
+        }
+    }
+
+    public class ViewModel
+    {
+        private ObservableCollection<Book> books = new ObservableCollection<Book>();
+
+        public IEnumerable<Book> Books => books;
+
+        public ViewModel()
+        {
+            books.Add(new Book() { Author= "F. Scott Fitzgerald", Title= "The Great Gatsby", Description = "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan."});
+        }
+
+    }
+
+    
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; } 
+        public string Description { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Author} {Title} {Description} {ReleaseDate.Year}";
         }
     }
 }
